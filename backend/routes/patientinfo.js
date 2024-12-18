91e383f523a54f2ab02d0d5458fc36830e78b031
sourov129 <67619797+sourov129@users.noreply.github.com>
2024-12-18 16:14:39 +0600

    backend
    create, update, delete, fetch patient information

--- backend/routes/patientinfo.js
+++ backend/routes/patientinfo.js
@@ -11,13 +11,24 @@ const fetchuser = require("../middleware/fetchuser");
 // })
 //
 
+
 //Router1: Fetch All Patient Information admin only using "/api/patient/fetchpatient"
 
 router.get("/fetchpatient", fetchuser,async (req, res) => {
 
+
+
+
     try{
 
+
         const patientsInfo = await Patient.find();
+
+        // Check if patient exists
+        if (!patientsInfo || patientsInfo.length === 0) {
+            return res.status(404).json({ error: 'No patients found' });
+        }
+
         res.json({success: true, patientsInfo});
 
     }
@@ -83,6 +94,85 @@ router.post('/addpatient' ,fetchuser,[
 
 
 });
+
+//Route 3: update Patient Info For admin uisng "/api/patient/updatepatient"
+
+router.put('/updatepatient/:id', fetchuser,[
+
+], async (req, res) => {
+    const { patientName, dateOfBirth, patientGender, patientMobile, presentAddress, permanentAddress } = req.body;
+
+    // Check if patient exists
+    const patient = await Patient.findById(req.params.id);
+    if (!patient) {
+        return res.status(404).json({ error: 'Patient not found' });
+    }
+
+    try{
+        const age = moment().diff(moment(dateOfBirth), 'years');
+
+        patient.patientName = patientName || patient.patientName;
+        patient.dateOfBirth = dateOfBirth || patient.dateOfBirth;
+        patient.patientAge = age;
+        patient.patientGender = patientGender || patient.patientGender;
+        patient.patientMobile = patientMobile || patient.patientMobile;
+        patient.presentAddress = presentAddress || patient.presentAddress;
+        patient.permanentAddress = permanentAddress || patient.permanentAddress;
+
+        const updatedPatient = await patient.save();
+        res.json({ success: true, updatedPatient });
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({ errors: "Internal Server Error" });
+    }
+})
+
+
+//Delete Information fro admin  using "/api/patient/deletepatient"
+
+router.delete('/deletepatient/:id',fetchuser, async (req, res) => {
+
+    const patient = await Patient.findById(req.params.id);
+    if (!patient) {
+        return res.status(404).json({ error: 'Patient not found' });
+    }
+
+    try{
+        await Patient.findByIdAndDelete(req.params.id);
+        res.json({success: true, message: 'Patient deleted successfully'});
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({ errors: "Internal Server Error" });
+    }
+
+});
+
+
+//Route4: View Patient details by user using "/api/patient/viewpatient" (No authentication required)
+
+router.get('/viewpatient', async (req, res) => {
+
+    try{
+        const patientsInfo = await Patient.find({}, '-_id -dateOfBirth');
+
+        // Check if patient exists
+        if (!patientsInfo || patientsInfo.length === 0) {
+            return res.status(404).json({ error: 'No patients found' });
+        }
+
+        res.json({success: true, patientsInfo});
+
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({errors: "Internal Server Error"});
+    }
+})
+
+
+
 
 
 
